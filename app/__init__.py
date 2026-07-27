6e8d66eefa3ca917b7d7f137ffb03873a88a70f4
Suraj Shinde <shindesuraj8748@gmail.com>
2026-07-28 02:01:56 +0530

    Prepare application for first deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_migrate import Migrate
+
 from config import Config
-from app.routes import main
 from app.database import db
+from app.routes.home import main
+from app.models import Plant
+
+migrate = Migrate()
 
 
 def create_app():
@@ -9,6 +14,7 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     app.register_blueprint(main)
 
---
+++ app/models/plant.py
@@ -0,0 +1,18 @@
+from app.database import db
+from datetime import datetime
+
+
+class Plant(db.Model):
+    __tablename__ = "plants"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+
+    category = db.Column(db.String(50), nullable=False)
+
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+
+    quantity = db.Column(db.Integer, nullable=False, default=0)
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
