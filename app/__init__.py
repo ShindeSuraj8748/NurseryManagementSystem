4a9a107d7bb84ce82e811153dee3c760f60d6b7f
Suraj Shinde <shindesuraj8748@gmail.com>
2026-07-28 00:11:38 +0530

    Add template structure and homepage

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
@@ -6,6 +6,6 @@ def create_app():
 
     @app.route("/")
     def home():
-        return "Welcome to Nursery Management System 🌱"
+        return render_template("index.html")
 
     return app
