39bb08bf727a9a74e3a91d68e2d666b9df77e62c
Suraj Shinde <shindesuraj8748@gmail.com>
2026-07-28 00:48:39 +0530

    Configure PostgreSQL with SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from config import Config
-from app.database import db
 from app.routes import main
+from app.database import db
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
+
     db.init_app(app)
 
     app.register_blueprint(main)
