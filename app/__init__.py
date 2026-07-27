a0585eb70f98c5d0dd6f016ef6b3c1952893656c
Suraj Shinde <shindesuraj8748@gmail.com>
2026-07-28 00:26:06 +0530

    Integrate SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
+from config import Config
+from app.database import db
+from app.routes import main
 
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
 
-    @app.route("/")
-    def home():
-        return render_template("index.html")
+    app.register_blueprint(main)
 
     return app
